45039dcbac664037ca550fcf6922b46832dd0b33
Svalenzuelac23 <svalenzuelac23@gmail.com>
2020-12-03 20:59:45 -0600

    First Commit

--- src/03. Persistence/Persistence/Extension.cs
+++ src/03. Persistence/Persistence/Extension.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Data;
 
 namespace Persistence
 {
@@ -7,6 +10,16 @@ namespace Persistence
         public static IServiceCollection AddPersistence(this IServiceCollection services)
         {
             //Add services to use in Persistence
+            IConfiguration configuration;
+            using (var serviceProvider = services.BuildServiceProvider())
+            {
+                configuration = serviceProvider.GetService<IConfiguration>();
+            }
+
+            services.AddDbContext<ApplicationDbContext>(option =>
+            {
+                option.UseSqlServer(configuration.GetConnectionString("{your-conection-string}"));
+            });
             return services;
         }
     }
